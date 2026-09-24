08078da9326fff2ee5bbe2c1fe99e6b7ecf70d3d
kamrontelegrambot-afk <kamrontelegrambot@gmail.com>
2026-09-24 09:28:27 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8461765377:AAFPGCg_Lecx-hAeMRQt3sf3v8wbyHRh4ns")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "8959542305"))
+DATABASE_URL = os.getenv("DATABASE_URL", "")
