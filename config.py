ad7f6b4704758c32e2170483d59a66b68622390e
kamrontelegrambot-afk <kamrontelegrambot@gmail.com>
2026-09-24 09:34:55 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8461765377:AAFPGCg_Lecx-hAeMRQt3sf3v8wbyHRh4ns")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "8959542305"))
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://baza_1xvo_user:fxIMFYHAUlYF7lOGWzrtxoset7IgI2sb@dpg-daqag4dg1s2s73fp1usg-a.oregon-postgres.render.com/baza_1xvo")
