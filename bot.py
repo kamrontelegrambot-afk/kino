79277bf9bc367cf85531afa9e2de0bffc547b76c
kamrontelegrambot-afk <kamrontelegrambot@gmail.com>
2026-09-26 15:04:09 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,9 +99,9 @@ if not RENDER_EXTERNAL_HOSTNAME:
 WEBHOOK_URL = f"https://{RENDER_EXTERNAL_HOSTNAME}{WEBHOOK_PATH}"
 
 # ======================== Bot sozlamalari ========================
-BOT_USERNAME = "Kinomarioo_bot"
-CHANNEL_USERNAME = "@kinomario_kino"
-CHANNEL_URL = "https://t.me/kinomario_kino"
+BOT_USERNAME = "@Kinolarolami7bot"
+CHANNEL_USERNAME = "@kinolar_olami_i7"
+CHANNEL_URL = "https://t.me/kinolar_olami_i7"
 
 
 # ======================== SELF-PING ========================
@@ -964,8 +964,8 @@ async def handle_code(update: Update, context: CallbackContext):
             await update.message.reply_text("❌ Video yuborishda xatolik yuz berdi.")
             return
         links_msg = (
-            f"📱 Instagram: https://www.instagram.com/kinomar1o\n"
-            f"📣 Kino kanal: @kinomario_kino {CHANNEL_USERNAME}"
+            f"📱 Instagram: https://www.instagram.com/kino_sevarlar\n"
+            f"📣 Kino kanal: @kinolar_olami_i7 {CHANNEL_USERNAME}"
         )
         await update.message.reply_text(links_msg)
         safe_task(send_ad(context.bot, user_id))
